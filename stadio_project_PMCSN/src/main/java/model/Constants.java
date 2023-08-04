70453229ea7894e2a88b08f086cc073578ce75a8
Luca Saverio Esposito <esposito.1917959@studenti.uniroma1.it>
2023-08-04 12:43:57 +0200

    added constant values and event types.

---
+++ stadio_project_PMCSN/src/main/java/model/Constants.java
@@ -0,0 +1,58 @@
+package model;
+
+public class Constants {
+
+    // ---- VIP USER PROB ----
+    public static final  double q = 0.076438;
+
+    // ---- ABANDON PROB ----
+    // probability of abandon after ticket check
+    public static final double P1 = 0.08;
+
+    // probability of abandon after first perquisition
+    public static final double P2 = 0.02;
+
+    // probability of abandon after second perquisition
+    public static final double P3 = 0.02;
+
+    // probability of abandon after vip ticket check
+    public static final double P6 = 0.02;
+
+    // probability of abandon after vip perquisition
+    public static final double P7 = 0.02;
+
+    // ---- SKIPP PROB ----
+    // probability of skipping first perquisition
+    public static final double P4 = 0.3;
+
+    // probability of skipping second perquisition
+    public static final double P5 = 0.3;
+
+    // ---- ARRIVAL RATES [req/sec]----
+    // first time window rate
+    public static final double LAMBDA1 = 0.24044;
+
+    // second time window rate
+    public static final double LAMBDA2 = 0.84154;
+
+    // third time window rate
+    public static final double LAMBDA3 = 0.12022;
+
+    // ---- SERVICE RATES  [sec] ----
+    // ticket check
+    public static final double TC_SR = 20;
+
+    // perquisition
+    public static final double P_SR = 30;
+
+    // turnstiles
+    public static final double T_SR = 10;
+
+    // VIP ticket check
+    public static final double V_TC_SR = 40;
+
+    // VIP perquisition
+    public static final double V_P_SR = 60;
+
+
+}
---
+++ stadio_project_PMCSN/src/main/java/model/Events.java
@@ -0,0 +1,46 @@
+package model;
+
+public class Events {
+    //---- VIP EVENTS ----
+    // Ticket check
+    public static final int ARRIVAL_EVENT_VIP_TICKET = 1;
+    public static final int DEPARTURE_EVENT_VIP_TICKET = 1;
+    public static final int ABBOND_EVENT_VIP_TICKET = 1;
+
+    // Perquisition
+    public static final int ARRIVAL_EVENT_VIP_PERQUISIION = 1;
+    public static final int DEPARTURE_EVENT_VIP_PERQUISITION = 1;
+    public static final int ABBOND_EVENT_VIP_PERQUISITION = 1;
+
+
+    //---- NOT VIP EVENTS ----
+    // Ticket check
+    public static final int ARRIVAL_EVENT_TICKET = 1;
+    public static final int DEPARTURE_EVENT_TICKET = 1;
+    public static final int ABBOND_EVENT_TICKET = 1;
+
+    // First perquisition
+    public static final int ARRIVAL_EVENT_FIRST_PERQUISIION = 1;
+    public static final int DEPARTURE_EVENT_FIRST_PERQUISITION = 1;
+    public static final int ABBOND_EVENT_FIRST_PERQUISITION = 1;
+
+    // Turnstiles
+    public static final int ARRIVAL_EVENT_TURNSTILES = 1;
+    public static final int DEPARTURE_EVENT_TURNSTILES = 1;
+
+
+    // Second perquisition
+    public static final int ARRIVAL_EVENT_SECOND_PERQUISIION = 1;
+    public static final int DEPARTURE_EVENT_SECOND_PERQUISITION = 1;
+    public static final int ABBOND_EVENT_SECOND_PERQUISITION = 1;
+
+    // ---- ALL EVENTS ----
+    public static final int ALL_EVENTS_VIP_TICKET = ARRIVAL_EVENT_VIP_TICKET + DEPARTURE_EVENT_VIP_TICKET + ABBOND_EVENT_VIP_TICKET;
+    public static final int ALL_EVENTS_VIP_PERQUISITION = ARRIVAL_EVENT_VIP_PERQUISIION + DEPARTURE_EVENT_VIP_PERQUISITION + ABBOND_EVENT_VIP_PERQUISITION;
+    public static final int ALL_EVENTS_TICKET = ARRIVAL_EVENT_TICKET + DEPARTURE_EVENT_TICKET + ABBOND_EVENT_TICKET;
+    public static final int ALL_EVENTS_FIRST_PERQUISITION = ARRIVAL_EVENT_FIRST_PERQUISIION + DEPARTURE_EVENT_FIRST_PERQUISITION + ABBOND_EVENT_FIRST_PERQUISITION;
+    public static final int ALL_EVENTS_TURNSTILES = ARRIVAL_EVENT_TURNSTILES + DEPARTURE_EVENT_TURNSTILES ;
+    public static final int ALL_EVENTS_SECOND_PERQUISITION = ARRIVAL_EVENT_SECOND_PERQUISIION + DEPARTURE_EVENT_SECOND_PERQUISITION + ABBOND_EVENT_SECOND_PERQUISITION;
+
+
+}
